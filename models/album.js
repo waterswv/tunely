5ccd07cb4dd0708a5ffb3d78ac9f7e6d7cdb90d2
Bryan <bryanmierke-macbookair@Bryans-MacBook-Air.local>
2017-07-21 10:46:22 -0700

    added a song.js file, created schema on song, required song in albums.js, exported to index.js in models, and added songSchema to Album Schema.

--- models/album.js
+++ models/album.js
@@ -1,11 +1,19 @@
 var mongoose = require("mongoose");
+
 var Schema = mongoose.Schema;
+
+var Song = require('./song.js');
+
+
+
+
 
 var AlbumSchema = new Schema({
   artistName: String,
   name: String,
   releaseDate: String,
-  genres: [ String ]
+  genres: [ String ],
+  song: [songSchema]
 });
 
 var Album = mongoose.model('Album', AlbumSchema);
--- models/index.js
+++ models/index.js
@@ -2,5 +2,6 @@ var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/tunely_test");
 
 var Album = require('./album');
+var Song = require('./song');
 
 module.exports.Album = Album;
